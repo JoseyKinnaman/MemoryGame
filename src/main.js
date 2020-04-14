9353c2544e1f1d194ecaa860b31081f403b3f858
Jozy Kinnaman <jozypants@gmail.com>
2020-04-13 17:41:27 -0700

    update img to index.html.

--- src/main.js
+++ src/main.js
@@ -1,2 +1,10 @@
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./styles.css";
 import $ from "jquery";
+
+$(document).ready(function() {
+  $(".card").click(function() {
+    
+  })
+})
